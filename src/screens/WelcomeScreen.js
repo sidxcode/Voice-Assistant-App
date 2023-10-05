3e9d3ed662f2edce599566771fad2192851ee84c
Siddharth Borman <siddharth02b@gmail.com>
2023-10-05 14:52:18 +0530

    welcomeScreen added

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,24 +1,25 @@
 import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
-// import tw from 'twrnc';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { useNavigation } from '@react-navigation/native';
 
 export default function WelcomeScreen() {
+    const navigation = useNavigation();
   return (
     <SafeAreaView className = "flex-1 flex justify-around bg-white">
       <View className = "space-y-2" >
-        <Text className = "text-center font-bold text-gray-700" >
+        <Text style={{fontSize: wp(10)}} className = "text-center font-bold text-gray-700" >
             Jarvis
         </Text>
-        <Text className = "text-center text-gray-600 font-semibold text-base tracking-wider" >
+        <Text style={{fontSize: wp(4)}} className = "text-center text-gray-600 font-semibold tracking-wider" >
             The future is here, powered by AI.
         </Text>
       </View>
       <View className="flex-row justify-center">
-        <Image source={require('../../assets/img/welcome.png')} className="w-72 h-72"></Image>
+        <Image source={require('../../assets/img/welcome.png')} style={{width: wp(75), height: wp(75)}}></Image>
       </View>
-      <TouchableOpacity className="bg-emerald-600 mx-5 p-4 rounded-2xl">
-        <Text className= "text-center font-bold text-white text-2xl">Get Started</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')} className="bg-emerald-600 mx-5 p-4 rounded-2xl">
+        <Text style={{fontSize: wp(6)}} className= "text-center font-bold text-white">Get Started</Text>
       </TouchableOpacity>
     </SafeAreaView>
   )
