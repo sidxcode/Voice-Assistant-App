9a0ba9f177154c4646ad60f8ae05b253bc25acee
Siddharth Borman <siddharth02b@gmail.com>
2023-10-05 11:41:00 +0530

    added navigation

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import tw from 'twrnc';
+import AppNavigation from './src/navigation/index';
+
+export default function App() {
+  return (
+    <AppNavigation/>
+  )
+}
