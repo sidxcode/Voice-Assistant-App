6dbe87ed7ed09c9acddb3a9000f4dcb36ca893c9
Siddharth Borman <siddharth02b@gmail.com>
2023-10-05 14:52:43 +0530

    initital HomeScreen added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import React, { useState } from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+    const [messages, setMessages] = useState([]);
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+        <SafeAreaView className="flex-1 flex mx-5">
+            {/* bot icon */}
+            <View className="flex-row justify-center">
+                <Image source={require('../../assets/img/bot.png')} style={{height: hp(15), width: hp(15)}}/>
+            </View>
+
+            {/*features || messages */}
+            {
+                messages.length>0? (
+                    <View>
+                    </View>
+                ): (
+                    <Features />
+                )
+            }
+        </SafeAreaView>
     </View>
   )
 }
