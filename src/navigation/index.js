34d83a0b41a131b495b453293dececdcee38b10d
Siddharth Borman <siddharth02b@gmail.com>
2023-10-05 11:58:21 +0530

    fixed navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,5 @@
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
@@ -6,13 +7,15 @@ import WelcomeScreen from '../screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
-function AppNavigation(){
-    return (   
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Welcome'>
-                <Stack.Screen name = "Home" component={HomeScreen}/>
-                <Stack.Screen name = "Welcome" component={WelcomeScreen}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Welcome'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default AppNavigation;
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import tw from 'twrnc';
 
 export default function WelcomeScreen() {
   return (
-    <View>
+    <SafeAreaView class = {tw`flex-1 flex justify-around bg-white`}>
       <Text>WelcomeScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
